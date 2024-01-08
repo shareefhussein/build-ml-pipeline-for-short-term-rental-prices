1a23a3d7a7f8a8425d850cf16526a27a13914e31
Shareef.Mansour <Shareef.Mansour@jo.zain.com>
2024-01-08 21:43:32 +0300

    new

--- src/get_data/run.py
+++ src/get_data/run.py
@@ -8,7 +8,7 @@ import os
 
 import wandb
 
-from wandb_utils.log_artifact import log_artifact
+# from wandb_utils.log_artifact import log_artifact
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -21,7 +21,7 @@ def go(args):
 
     logger.info(f"Returning sample {args.sample}")
     logger.info(f"Uploading {args.artifact_name} to Weights & Biases")
-    log_artifact(
+    wandb.log_artifact(
         args.artifact_name,
         args.artifact_type,
         args.artifact_description,
